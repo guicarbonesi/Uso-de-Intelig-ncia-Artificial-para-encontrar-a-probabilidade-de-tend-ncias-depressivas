1510ffd0ce41125f7213af13864d1d0ceb4d552d
moreaux96 <31132557+moreaux96@users.noreply.github.com>
2022-12-12 12:14:57 -0300

    subindo att
    att

--- cleansingdata.py
+++ cleansingdata.py
@@ -4,19 +4,15 @@ import pandas as pd
 import string
 import nltk
 
-#import dataset
+
 dataset_columns = ["target", "ids", "date", "flag", "user", "text"]
 dataset_encode = "ISO-8859-1"
-data = pd.read_csv("training.1600000.processed.noemoticon.csv", encoding = dataset_encode, names = dataset_columns)
+data = pd.read_csv("dataset_random", encoding = dataset_encode, names = dataset_columns)
 
-#hapus kolom lainnya, hanya mengambil kolom target dan text
 data.drop(['ids','date','flag','user'],axis = 1,inplace = True)
 
-#cek data null
 data['text'].isnull().sum()
-#hasilnya 0
 
-#menghilangkan punctuation
 def remove_punctuation(text):
     no_punct=[words for words in text if words not in string.punctuation]
     words_wo_punct=''.join(no_punct)
@@ -24,17 +20,9 @@ def remove_punctuation(text):
 data['clean_text']=data['text'].apply(lambda x: remove_punctuation(x))
 data.head()
 
-#menghilangkan hyperlink
-data['clean_text'] = data['clean_text'].str.replace(r"http\S+", "") 
-data.head()
-
-#menghilangkan emoji
 data['clean_text'] = data['clean_text'].str.replace('[^\w\s#@/:%.,_-]', '', flags=re.UNICODE)
-
-#buat semua kata jadi lowercase
 data['clean_text'] = data['clean_text'].str.lower()
 
-#tokenization
 nltk.download('punkt')
 
 def tokenize(text):
@@ -42,9 +30,9 @@ def tokenize(text):
     return split
 data['clean_text_tokenize']=data['clean_text'].apply(lambda x: tokenize(x.lower()))
 
-#stopwords
+
 nltk.download('stopwords')
-stopword = nltk.corpus.stopwords.words('english')
+stopword = nltk.corpus.stopwords.words('portuguese')
 
 def remove_stopwords(text):
     text=[word for word in text if word not in stopword]
